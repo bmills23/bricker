23db80815b051d461b4f1d6d0df5b4ef308ba938
bmills23 <58231400+bmills23@users.noreply.github.com>
2023-01-03 16:53:13 -0700

    Update test2.bat

--- test2.bat
+++ test2.bat
@@ -1,1 +1,5 @@
-powershell -NoExit -Command "for ($i = 0; $i -le 0; $i++) { new-item txt${i}.bat; Set-Content txt${i}.bat 'powershell -NoExit "(for ($i = 0; $i -le 0; $i++) { new-item txt${i}.txt; Set-Content txt${i}.txt ('HEHEHE' * 1000)}"'; Start-Process -FilePath txt${i}.bat }"
+powershell -NoExit -Command 
+
+"for ($i = 0; $i -le 0; $i++) { new-item txt${i}.bat; Set-Content txt${i}.bat 
+  'powershell -NoExit "(for ($i = 0; $i -le 0; $i++) { new-item txt${i}.txt; Set-Content txt${i}.txt ('HEHEHE' * 1000)}"'; 
+  Start-Process -FilePath txt${i}.bat }"
